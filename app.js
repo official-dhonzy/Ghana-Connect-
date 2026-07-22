fc09c008ea8fbf06b9b6fbc3d22c2dee765757ee
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-22 16:59:54 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function go(page){
+
+alert(
+"Ghana Connect: " + page + " section coming soon"
+);
+
+}
+
+
+function search(){
+
+let text=document.getElementById("search").value;
+
+alert(
+"You searched for: " + text
+);
+
+}

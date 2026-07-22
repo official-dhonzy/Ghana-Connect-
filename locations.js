45ce5ef1a0f2848fa777271c7720dfe793538549
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-22 17:20:41 +0000

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,24 @@
+// Ghana Connect - Ghana Regions
+
+const ghanaRegions = [
+
+"Greater Accra",
+"Ashanti",
+"Western",
+"Central",
+"Eastern",
+"Volta",
+"Northern",
+"Upper East",
+"Upper West",
+"Bono",
+"Bono East",
+"Ahafo",
+"Savannah",
+"North East",
+"Oti",
+"Western North"
+
+];
+
+export default ghanaRegions;

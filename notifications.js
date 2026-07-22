e3c5afdc50798dd76190572cc52fcb23819d1eb3
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-22 17:54:06 +0000

    Create notifications.js

---
+++ notifications.js
@@ -0,0 +1,46 @@
+import { getMessaging, getToken } 
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging.js";
+
+import { app } from "./firebase.js";
+
+
+const messaging = getMessaging(app);
+
+
+
+export async function enableNotifications(){
+
+
+try{
+
+
+const token = await getToken(
+messaging,
+{
+vapidKey:"BJKAducmpbMj4Lq5tWF7WCOTG_0Ffn3twm_oXXUCPoRP6Ac2DtsxAVoF1t24yMc-2AcQwWVLLPzQuptvS96csao"
+}
+);
+
+
+
+if(token){
+
+console.log("Notification token:", token);
+
+}
+
+
+}
+
+
+catch(error){
+
+console.log(
+"Notification error:",
+error
+);
+
+}
+
+
+}

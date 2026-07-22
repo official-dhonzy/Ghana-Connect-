eab8340b81655514ce662614f8aab1a640dbff70
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-22 23:35:56 +0000

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -14,20 +14,46 @@ export async function enableNotifications(){
 try{
 
 
-const token = await getToken(
-messaging,
-{
-vapidKey:"BJKAducmpbMj4Lq5tWF7WCOTG_0Ffn3twm_oXXUCPoRP6Ac2DtsxAVoF1t24yMc-2AcQwWVLLPzQuptvS96csao"
+const permission = await Notification.requestPermission();
+
+
+
+if(permission !== "granted"){
+
+console.log("Notification permission denied");
+
+return;
+
 }
+
+
+
+const token = await getToken(
+
+messaging,
+
+{
+
+vapidKey:
+"BJKAducmpbMj4Lq5tWF7WCOTG_0Ffn3twm_oXXUCPoRP6Ac2DtsxAVoF1t24yMc-2AcQwWVLLPzQuptvS96csao"
+
+}
+
 );
 
 
 
 if(token){
 
-console.log("Notification token:", token);
+
+console.log(
+"Notification token:",
+token
+);
+
 
 }
+
 
 
 }
@@ -35,10 +61,12 @@ console.log("Notification token:", token);
 
 catch(error){
 
+
 console.log(
 "Notification error:",
 error
 );
+
 
 }
 

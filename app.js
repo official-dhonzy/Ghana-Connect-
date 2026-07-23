2656063dcb3f08289fca8df3137f6186a97bc103
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-23 22:45:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 function go(page){
 
-alert(
-"Ghana Connect: " + page + " section coming soon"
-);
+  window.location.href = page + ".html";
 
 }
 
 
 function search(){
 
-let text=document.getElementById("search").value;
+  let text = document.getElementById("search").value;
 
-alert(
-"You searched for: " + text
-);
+  alert(
+    "You searched for: " + text
+  );
 
 }
-// Android back button support
-window.addEventListener("popstate", function () {
-  if (window.history.length > 1) {
-    history.back();
-  }
-});
 
-// Create browser history when moving pages
-function goBack() {
-  if (history.length > 1) {
+
+// Back button support
+function goBack(){
+
+  if(history.length > 1){
+
     history.back();
+
   } else {
+
     window.location.href = "index.html";
+
   }
+
 }
+
+
+// Browser navigation support
+window.addEventListener("popstate", function(){
+
+  console.log("Going back");
+
+});
